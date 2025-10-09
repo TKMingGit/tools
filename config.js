bf0645c5d75c3ee0a472b7f23691fc6e630fe75b
赵明 <zm461060@outlook.com>
2025-10-09 14:58:44 +0800

    task:优化点位列表时间字段title

--- config.js
+++ config.js
@@ -8,8 +8,5 @@ module.exports = {
     dev: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
     test: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
     pre: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1"
-  },
-
-  // 高德地图API密钥
-  gdKKey: "d604e181c6929b3e48ad62deeddae7f4"
+  }
 };
