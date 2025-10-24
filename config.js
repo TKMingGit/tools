aafd5bec0bf9a7cb7d323641a7da69195113e1ce
赵明 <zm461060@outlook.com>
2025-10-24 10:11:24 +0800

    task:mongo链接配置改为域名方式

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   // MongoDB 配置
   mongoConfigs: {
     local: "mongodb://localhost:27017/gps",
-    dev: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
-    test: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
-    pre: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@10.0.95.24:27017,10.0.95.25:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1"
+    dev: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@mongoCluster1.xtm.cn:27017,mongoCluster2.xtm.cn:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
+    test: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@mongoCluster1.xtm.cn:27017,mongoCluster2.xtm.cn:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1",
+    pre: "mongodb://gpsdbuser:HxmZGKYCSm7gYLhX@mongoCluster1.xtm.cn:27017,mongoCluster2.xtm.cn:27017/gps?authSource=admin&authMechanism=SCRAM-SHA-1"
   }
 };
